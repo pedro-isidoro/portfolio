1393c8d4ade523d11c03ef4db5ece1f3c335ef60
Pedro <pedro.isidoro26@yahoo.com.br>
2023-11-10 15:42:09 -0300

    Refatorando o skills.jsx

--- src/components/pages/skills/Skills.jsx
+++ src/components/pages/skills/Skills.jsx
@@ -2,37 +2,7 @@ import './skills.css'
 
 function Skills (){
     return(
-        <section className='skills flex-column main-card main-skills-card'>
-            <div className='languages-skills flex-row'>
-                <div className="languages flex-column">
-                    <h2 className='heading-2'>Linguagens de Programação</h2>
-                    <ul>
-                        <li className="html">
-                            <a className="html" href="https://www.w3schools.com/html/" target='_blank'><i className='html fab fa-html5 html'/>HTML5</a>
-                        </li>
-                        <li className="css">
-                            <a className="css" href="https://www.w3schools.com/css/" target="_blank"><i className='css fab fa-css3-alt css'/>CSS3</a>
-                        </li>
-                        <li className="js">
-                            <a className="js" href="https://www.w3schools.com/js/" target="_blank"><i className='js fab fa-js-square js'/>JavaScript (Intemediário)</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="library flex-column">
-                    <h2 className='heading-2'>Bibliotecas</h2>
-                    <ul>
-                        <li className="library-hover react blue-color">
-                            <a className='blue-color library-hover' href="https://react.dev/" target='_blank'><i className="blue-color fab fa-react library-hover" />React (Intermediário)</a>
-                        </li>
-                        <li className="library-hover react-hook-form blue-color">
-                            <a className='blue-color library-hover' href="https://react-hook-form.com/" target='_blank'>React Hook Form (Básico)</a>
-                        </li>
-                        <li className="library-hover react-rounter blue-color">
-                            <a className='blue-color library-hover' href="https://reactrouter.com/en/main" target='_blank'>React Router (Básico)</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <section className='skills flex-row main-card main-skills-card'>
             <div className='soft-skills flex-column'>
                 <h2 className='heading-2'>Competências Comportamentais</h2>
                 <ul>
@@ -42,6 +12,63 @@ function Skills (){
                     <li className='blue-color'>Esforço</li>
                     <li className='blue-color'>Muita vontade de aprender</li>
                     <li className='blue-color'>Flexibilidade</li>
+                </ul>
+            </div>
+            <div className='languages-skills flex-column'>
+                <h2 className='heading-2'>Competências Técnicas</h2>
+                <ul className='languages flex-row'>
+                    <li className="html flex-column">
+                        <div className='front'>
+                            <i className='html fab fa-html5 html'/>
+                            <p className='html'>HTML5</p>
+                        </div>
+                        <div className='back'>
+                            <p>É uma linguagens de marcação de hipertexto, usada principalmente para desenvolver páginas da web ou aplicativos.</p>
+                        </div>
+                    </li>
+                    <li className="css flex-column">
+                        <div className='front'>
+                            <i className='css fab fa-css3-alt css'/>
+                            <p className='css'>CSS3</p>
+                        </div>
+                        <div className='back'>
+                            <p>CSS é a linguagem que usamos para estilizar um documento HTML.<br/>CSS descreve como os elementos HTML devem ser exibidos.</p>
+                        </div>
+                    </li>
+                    <li className="js flex-column">
+                        <div className='front'>
+                            <i className='js fab fa-js-square js'/>
+                            <p className='js'>JavaScript <br/> (Intemediário)</p>
+                        </div>
+                        <div className='back'>
+                            <p>JavaScript é a linguagem de programação mais popular do mundo.<br/>JavaScript é a linguagem de programação da Web.</p>
+                        </div>
+                    </li>
+                    <li className="flex-column react blue-color">
+                        <div className='front'>
+                            <i className="blue-color fab fa-react library-hover" />
+                            <p className='blue-color'>React <br/> (Intermediário)</p>
+                        </div>
+                        <div className='back'>
+                            <p>É a biblioteca para interfaces de usuário web e nativas. <br/>Criando interfaces de usuário a partir de peças individuais chamadas componentes escritos em JavaScript.</p>
+                        </div>
+                    </li>
+                    <li className="flex-column react-hook-form blue-color">
+                        <div className='front'>
+                            <p className='blue-color'>React Hook Form <br/> (Básico)</p>
+                        </div>
+                        <div className='back'>
+                            <p>É uma biblioteca para formulários de alto desempenho, flexíveis e extensíveis com validação fácil de usar.</p>
+                        </div>
+                    </li>
+                    <li className="flex-column react-rounter blue-color">
+                        <div className='front'>
+                            <p className='blue-color'>React Router <br/> (Básico)</p>
+                        </div>
+                        <div className='back'>
+                            <p>React Router permite "roteamento do lado do cliente".<br/>Isso permite experiências de usuário mais rápidas.</p>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </section>
