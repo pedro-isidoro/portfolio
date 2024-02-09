cb62f9a10c79cab10d567218d85f61b16d65444d
Pedro <pedro.isidoro26@yahoo.com.br>
2024-02-09 08:30:46 -0300

    Update code

--- src/components/pages/mineSlide/Slide.jsx
+++ src/components/pages/mineSlide/Slide.jsx
@@ -3,8 +3,8 @@ import slideImage from "/src/images/slide-image.png"
 
 function Slide (){
     return(
-        <section className='about flex-row main-card main-about-card'>
-            <div className='about-text flex-column'>
+        <section className='slide-video about flex-row'>
+            <div className='slide-div about-text flex-column'>
                 <h2 className='heading-2'>Um pouco mais sobre minha vida</h2>
                 <video controls width="750" height="500" poster={slideImage}>    
                     <source src={slideVideo} type="video/mp4" />
